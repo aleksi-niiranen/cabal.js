33d56ca0cb77a1ca641926d141d43cfb998b3eb1
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2014-06-26 17:48:51 +0300

    update sample data structure

--- property-mappings.js
+++ property-mappings.js
@@ -46,101 +46,101 @@ var caballist = (function () {
 
 var results = [
     { Cells: { results:
-        [{ Name: 'Title', Value: 'Provision of Customer Relationship Management System', Type: 'Edm.String' },
-         { Name: 'Customer', Value: 'Carmen May', Type: 'Edm.String' },
-         { Name: 'DateOfAction', Value: (new Date(2014, 6, 24)).toISOString(), Type: 'Edm.DateTime' },
-         { Name: 'SiteName', Value: 'http://www.google.com', Type: 'Edm.String' },
-         { Name: 'ProjectStatus', Value: 'In Progress', Type: 'Edm.String' }]
+        [{ Key: 'Title', Value: 'Provision of Customer Relationship Management System', Type: 'Edm.String' },
+         { Key: 'Customer', Value: 'Carmen May', Type: 'Edm.String' },
+         { Key: 'DateOfAction', Value: (new Date(2014, 6, 24)).toISOString(), Type: 'Edm.DateTime' },
+         { Key: 'SiteName', Value: 'http://www.google.com', Type: 'Edm.String' },
+         { Key: 'ProjectStatus', Value: 'In Progress', Type: 'Edm.String' }]
     }},
 
     { Cells: { results:
-        [{ Name: 'Title', Value: 'Framework for Executive Search and Recruitment', Type: 'Edm.String' },
-         { Name: 'Customer', Value: 'Joshua Harris', Type: 'Edm.String' },
-         { Name: 'DateOfAction', Value: (new Date(2014, 7, 7)).toISOString(), Type: 'Edm.DateTime' },
-         { Name: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
-         { Name: 'ProjectStatus', Value: 'Onhold', Type: 'Edm.String' }],
+        [{ Key: 'Title', Value: 'Framework for Executive Search and Recruitment', Type: 'Edm.String' },
+         { Key: 'Customer', Value: 'Joshua Harris', Type: 'Edm.String' },
+         { Key: 'DateOfAction', Value: (new Date(2014, 7, 7)).toISOString(), Type: 'Edm.DateTime' },
+         { Key: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
+         { Key: 'ProjectStatus', Value: 'Onhold', Type: 'Edm.String' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'Title', Value: 'Innovative Ship to Shore Ferry Charging Solution', Type: 'Edm.String' },
-         { Name: 'Customer', Value: 'Mario Castro', Type: 'Edm.String' },
-         { Name: 'DateOfAction', Value: (new Date(2014, 9, 28)).toISOString(), Type: 'Edm.DateTime' },
-         { Name: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
-         { Name: 'ProjectStatus', Value: 'In Progress', Type: 'Edm.String' }],
+        [{ Key: 'Title', Value: 'Innovative Ship to Shore Ferry Charging Solution', Type: 'Edm.String' },
+         { Key: 'Customer', Value: 'Mario Castro', Type: 'Edm.String' },
+         { Key: 'DateOfAction', Value: (new Date(2014, 9, 28)).toISOString(), Type: 'Edm.DateTime' },
+         { Key: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
+         { Key: 'ProjectStatus', Value: 'In Progress', Type: 'Edm.String' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'Title', Value: 'Installation of Energy Efficiency Measures', Type: 'Edm.String' },
-         { Name: 'Customer', Value: 'Lonnie Hunt', Type: 'Edm.String' },
-         { Name: 'DateOfAction', Value: (new Date(2014, 8, 11)).toISOString(), Type: 'Edm.DateTime' },
-         { Name: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
-         { Name: 'ProjectStatus', Value: 'In Progress', Type: 'Edm.String' }],
+        [{ Key: 'Title', Value: 'Installation of Energy Efficiency Measures', Type: 'Edm.String' },
+         { Key: 'Customer', Value: 'Lonnie Hunt', Type: 'Edm.String' },
+         { Key: 'DateOfAction', Value: (new Date(2014, 8, 11)).toISOString(), Type: 'Edm.DateTime' },
+         { Key: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
+         { Key: 'ProjectStatus', Value: 'In Progress', Type: 'Edm.String' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'Title', Value: 'Replacement Pipework', Type: 'Edm.String' },
-         { Name: 'Customer', Value: 'Emma Howard', Type: 'Edm.String' },
-         { Name: 'DateOfAction', Value: (new Date(2014, 7, 11)).toISOString(), Type: 'Edm.DateTime' },
-         { Name: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
-         { Name: 'ProjectStatus', Value: 'Onhold', Type: 'Edm.String' }],
+        [{ Key: 'Title', Value: 'Replacement Pipework', Type: 'Edm.String' },
+         { Key: 'Customer', Value: 'Emma Howard', Type: 'Edm.String' },
+         { Key: 'DateOfAction', Value: (new Date(2014, 7, 11)).toISOString(), Type: 'Edm.DateTime' },
+         { Key: 'SiteName', Value: 'http://virta.baz', Type: 'Edm.String' },
+         { Key: 'ProjectStatus', Value: 'Onhold', Type: 'Edm.String' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'Title', Value: 'Broadcasting Equipment and Associated Infrastructure', Type: 'Edm.String' },
-         { Name: 'Customer', Value: 'George Hudson', Type: 'Edm.String' },
-         { Name: 'DateOfAction', Value: (new Date(2014, 6, 22)).toISOString(), Type: 'Edm.DateTime' },
-         { Name: 'SiteName', Value: 'http://mcninja.com', Type: 'Edm.String' },
-         { Name: 'ProjectStatus', Value: 'Late', Type: 'Edm.String' }]
+        [{ Key: 'Title', Value: 'Broadcasting Equipment and Associated Infrastructure', Type: 'Edm.String' },
+         { Key: 'Customer', Value: 'George Hudson', Type: 'Edm.String' },
+         { Key: 'DateOfAction', Value: (new Date(2014, 6, 22)).toISOString(), Type: 'Edm.DateTime' },
+         { Key: 'SiteName', Value: 'http://mcninja.com', Type: 'Edm.String' },
+         { Key: 'ProjectStatus', Value: 'Late', Type: 'Edm.String' }]
     }},
 ];
 
 var people = [
     { Cells: { results:
-        [{ Name: 'DisplayName', Value: 'Lydia Fernandez' },
-         { Name: 'PictureURL', Value: 'http://api.randomuser.me/portraits/women/94.jpg' },
-         { Name: 'Title', Value: 'User Experience Manager' },
-         { Name: 'Email', Value: 'lydia.fernandez12@example.com' },
-         { Name: 'Mobile', Value: '(317)-468-8105' }],
+        [{ Key: 'DisplayName', Value: 'Lydia Fernandez' },
+         { Key: 'PictureURL', Value: 'http://api.randomuser.me/portraits/women/94.jpg' },
+         { Key: 'Title', Value: 'User Experience Manager' },
+         { Key: 'Email', Value: 'lydia.fernandez12@example.com' },
+         { Key: 'Mobile', Value: '(317)-468-8105' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'DisplayName', Value: 'Jeff Bailey' },
-         { Name: 'PictureURL', Value: 'http://api.randomuser.me/portraits/men/80.jpg' },
-         { Name: 'Title', Value: 'Content Ninja' },
-         { Name: 'Email', Value: 'jeff.bailey91@example.com' },
-         { Name: 'Mobile', Value: '(169)-716-6932' }],
+        [{ Key: 'DisplayName', Value: 'Jeff Bailey' },
+         { Key: 'PictureURL', Value: 'http://api.randomuser.me/portraits/men/80.jpg' },
+         { Key: 'Title', Value: 'Content Ninja' },
+         { Key: 'Email', Value: 'jeff.bailey91@example.com' },
+         { Key: 'Mobile', Value: '(169)-716-6932' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'DisplayName', Value: 'Donald Gardner' },
-         { Name: 'PictureURL', Value: 'http://api.randomuser.me/portraits/men/15.jpg' },
-         { Name: 'Title', Value: 'Brand Manager' },
-         { Name: 'Email', Value: 'donald.gardner59@example.com' },
-         { Name: 'Mobile', Value: '(925)-983-8035' }],
+        [{ Key: 'DisplayName', Value: 'Donald Gardner' },
+         { Key: 'PictureURL', Value: 'http://api.randomuser.me/portraits/men/15.jpg' },
+         { Key: 'Title', Value: 'Brand Manager' },
+         { Key: 'Email', Value: 'donald.gardner59@example.com' },
+         { Key: 'Mobile', Value: '(925)-983-8035' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'DisplayName', Value: 'Joan Alexander' },
-         { Name: 'PictureURL', Value: 'http://api.randomuser.me/portraits/women/34.jpg' },
-         { Name: 'Title', Value: 'Lead Developer' },
-         { Name: 'Email', Value: 'joan.alexander27@example.com' },
-         { Name: 'Mobile', Value: '(698)-213-7104' }],
+        [{ Key: 'DisplayName', Value: 'Joan Alexander' },
+         { Key: 'PictureURL', Value: 'http://api.randomuser.me/portraits/women/34.jpg' },
+         { Key: 'Title', Value: 'Lead Developer' },
+         { Key: 'Email', Value: 'joan.alexander27@example.com' },
+         { Key: 'Mobile', Value: '(698)-213-7104' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'DisplayName', Value: 'Laurie Allen' },
-         { Name: 'PictureURL', Value: 'http://api.randomuser.me/portraits/women/45.jpg' },
-         { Name: 'Title', Value: 'Systems Engineer' },
-         { Name: 'Email', Value: 'laurie.allen93@example.com' },
-         { Name: 'Mobile', Value: '(924)-587-2336' }],
+        [{ Key: 'DisplayName', Value: 'Laurie Allen' },
+         { Key: 'PictureURL', Value: 'http://api.randomuser.me/portraits/women/45.jpg' },
+         { Key: 'Title', Value: 'Systems Engineer' },
+         { Key: 'Email', Value: 'laurie.allen93@example.com' },
+         { Key: 'Mobile', Value: '(924)-587-2336' }],
     }},
 
     { Cells: { results:
-        [{ Name: 'DisplayName', Value: 'Salvador Stevens' },
-         { Name: 'PictureURL', Value: 'http://api.randomuser.me/portraits/men/84.jpg' },
-         { Name: 'Title', Value: 'Infrastructure Specialist' },
-         { Name: 'Email', Value: 'salvador.stevens22@example.com' },
-         { Name: 'Mobile', Value: '(118)-454-1902' }]
+        [{ Key: 'DisplayName', Value: 'Salvador Stevens' },
+         { Key: 'PictureURL', Value: 'http://api.randomuser.me/portraits/men/84.jpg' },
+         { Key: 'Title', Value: 'Infrastructure Specialist' },
+         { Key: 'Email', Value: 'salvador.stevens22@example.com' },
+         { Key: 'Mobile', Value: '(118)-454-1902' }]
     }},
 ];
 
