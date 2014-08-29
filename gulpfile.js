d004f82cbad003d8d6b02fb6eb2d2c24f665a96e
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2014-08-29 19:17:17 +0300

    rename cabal.js to core.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
 
-gulp.task('concat', function () {
-    gulp.src(['./src/cabal.js', './src/components.js', './src/mapper.js'])
-        .pipe(concat('cabal.js'))
-        .pipe(gulp.dest('./dist/'));
-});
-
 gulp.task('build', function () {
-    gulp.src(['./src/cabal.js', './src/components.js', './src/mapper.js'])
+    gulp.src(['./src/core.js', './src/components.js', './src/mapper.js'])
         .pipe(concat('cabal.js'))
         .pipe(gulp.dest('./dist/'))
         .pipe(uglify())
