277e51bcee8a7a8030e1eb7d594963424e34f5fe
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2014-09-24 22:33:53 +0300

    Updated dist files

--- cabal-with-utils.js
+++ cabal-with-utils.js
@@ -92,3 +92,33 @@
     if (typeof root.cabal === 'undefined')
         root.cabal = cabal;
 })(this);
+
+(function (root) {
+    var utils = {};
+
+    var CabalProperty = function (name) {
+        this.name = name;
+        this.componentType = 'span';
+    };
+
+    CabalProperty.prototype.attributes = function (attributeMap) {
+        this.attr = attributeMap;
+        return this;
+    };
+
+    CabalProperty.prototype.as = function (type) {
+        if (type === undefined || type === null)
+            throw new Error("PropertyMapping's componentType can't be undefined or null");
+        this.componentType = type;
+        return this;
+    };
+
+    utils.mapper = function (name) {
+        var p = new CabalProperty(name);
+        return p;
+    };
+
+    if (typeof root.cabalutils === 'undefined')
+        root.cabalutils = utils;
+
+})(this);
